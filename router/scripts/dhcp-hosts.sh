e0f7256532c0163661681aaf98210a51d786dd12
Bob Fraser <bobfraser1@gmail.com>
2021-08-11 14:58:38 -0700

    Debian environment
    Signed-off-by: Bob Fraser <bobfraser1@gmail.com>

--- router/scripts/dhcp-hosts.sh
+++ router/scripts/dhcp-hosts.sh
@@ -5,5 +5,5 @@
 set -eu
 
 sed -i '/^dhcp-host=/d' /etc/dnsmasq.conf
-printf 'dhcp-host=08:00:27:00:00:10,alpine1,192.168.1.10,infinite\n' >> /etc/dnsmasq.conf
+printf 'dhcp-host=08:00:27:00:00:20,debian1,192.168.1.20,infinite\n' >> /etc/dnsmasq.conf
 /etc/init.d/dnsmasq restart
